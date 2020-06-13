8dfb3e667b45c78fb5765da3ce0f94733f555acd
jotarodri <44468803+jotarodri@users.noreply.github.com>
2020-06-13 12:53:21 +0200

    Add files via upload

--- keys.js
+++ keys.js
@@ -7,6 +7,10 @@ module.exports = {
         database: "heroku_be6dbf21fc15c3b"
     }
 
-    //'b4cfcb1591a818:e20bee0a@eu-cdbr-west-03.cleardb.net/heroku_be6dbf21fc15c3b?reconnect=true'
 
 };
+
+/*host: "localhost",
+       user: "root",
+       password: "",
+       database: "sportsconnection"*/
