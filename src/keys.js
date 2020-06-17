28348535bf0572dcd98fc9f2fd8971a199cde6f3
jotarodri <44468803+jotarodri@users.noreply.github.com>
2020-06-17 09:23:13 +0200

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-
-   host: "eu-cdbr-west-03.cleardb.net",
+database:{
+         host: "eu-cdbr-west-03.cleardb.net",
         user: "b8bc017858bec2",
         password: "36f1c644",
         database: "heroku_54a0fc249008a70"
