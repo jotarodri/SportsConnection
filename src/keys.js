381fa15623811ee766bb0bbeb9b9cb0f1300d3b2
jotarodri <44468803+jotarodri@users.noreply.github.com>
2020-06-17 08:58:20 +0200

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,11 +1,12 @@
 module.exports = {
 
     database: {
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "sportsconnection"
+        host: "b8bc017858bec2",
+        user: "36f1c644",
+        password: "eu-cdbr-west-03.cleardb.net",
+        database: "heroku_54a0fc249008a70"
     }
+    
 
 
 };
